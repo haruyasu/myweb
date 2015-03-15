be48871aeffc483733208dc99eea4c0cf1a73ade
haruyasu <harukun2002@gmail.com>
2015-03-15 22:46:16 +0900

    contactform

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,31 @@ jQuery(document).ready(function($) {
         entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
         }
     );
+
+    $("#modal-open").click(function() {
+    	$("body").append('<div id="modal-overlay"></div>');
+    	$("#modal-overlay").fadeIn("slow");
+    	centeringModalSyncer();
+    	$("#modal-content").fadeIn("slow");
+
+    	$("#modal-overlay,#modal-close").unbind().click(function() {
+    		$("#modal-content,#modal-overlay").fadeOut("slow",function() {
+    			$('#modal-overlay').remove();
+    		});
+            $('.form-control').val("");
+    	});
+    });
+
+    $(window).resize(centeringModalSyncer);
+
+	function centeringModalSyncer(){
+		var w = $(window).width();
+		var h = $(window).height();
+		var cw = $("#modal-content").outerWidth();
+		var ch = $("#modal-content").outerHeight();
+		$("#modal-content").css({
+            "left": ((w - cw) / 2) + "px",
+            "top": ((h - ch) / 2) + "px"
+        })
+	}
 });
